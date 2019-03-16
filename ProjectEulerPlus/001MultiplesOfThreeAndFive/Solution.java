978aaca5b791be2c67ead7a35521765637dd0473
Cramer Grimes <cramergrimes@gmail.com>
2019-03-15 20:10:54 -0400

    Update Solution.java

--- ProjectEulerPlus/001MultiplesOfThreeAndFive/Solution.java
+++ ProjectEulerPlus/001MultiplesOfThreeAndFive/Solution.java
@@ -7,20 +7,18 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-
-        int n = 100;
-
-        long threeTimes = 0;
-        long fiveTimes = 0;
-        long threeFiveInter = 0;
-
-        threeTimes = (n-1)/3;
-        System.out.println(threeTimes);
-        fiveTimes = (n-1)/5;
-        System.out.println(fiveTimes);
-        threeFiveInter = (n-1)/15;
-        System.out.println(threeFiveInter);
-        System.out.println( ((threeTimes*(threeTimes+1))/2) *3+ ((fiveTimes*(fiveTimes+1))/2) *5- ((threeFiveInter*(threeFiveInter+1))/2) *15);
-
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            long threeTimes = 0;
+            long fiveTimes = 0;
+            long threeFiveInter = 0;
+            
+            threeTimes = (n-1)/3;
+            fiveTimes = (n-1)/5;
+            threeFiveInter = (n-1)/15;
+            System.out.println( ((threeTimes*(threeTimes+1))/2) *3+ ((fiveTimes*(fiveTimes+1))/2) *5- ((threeFiveInter*(threeFiveInter+1))/2) *15);
+        }
     }
 }
